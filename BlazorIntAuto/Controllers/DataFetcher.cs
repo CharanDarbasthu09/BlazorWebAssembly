8f96cf103e2b2c0d6ab09ec95425ac9bd0befd66
Charan_Darbasthu <charan.darbasthu@agvance.net>
2024-02-01 13:33:11 +0530

    read images from json file

---
+++ BlazorIntAuto/Controllers/DataFetcher.cs
@@ -0,0 +1,17 @@
+﻿//using BlazorIntAuto.Data;
+
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace BlazorIntAuto.Controllers
+//{
+//  [Route("api/[controller]")]
+//  public class DataFetcher : Controller
+//  {
+//    [HttpGet("[action]")]
+//    public List<DashboardImage> GetData(string action, string id)
+//    {
+//      var str = File.ReadAllText("data.txt");
+//      return new List<DashboardImage>(str);
+//    }
+//  }
+//}
--- BlazorIntAuto/Data/DashboardImage.cs
+++ BlazorIntAuto/Data/DashboardImage.cs
@@ -1,9 +1,16 @@
-﻿namespace BlazorIntAuto.Data
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorIntAuto.Data
 {
   public class DashboardImage
   {
+    [JsonPropertyName("image")]
     public required string Image { get; set; }
+
+    [JsonPropertyName("info")]
     public required string Info { get; set; }
+
+    [JsonPropertyName("size")]
     public required string Size { get; set; }
   }
 }
